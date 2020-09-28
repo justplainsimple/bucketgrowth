27628e70a97a7c99c648378b4f0af0002c94a541
Scott Brown <scott@justplainsimple.com>
2020-09-27 17:00:20 -0700

    Closes #6. Adding JSON output support

--- cmd/bucketgrowth/main.go
+++ cmd/bucketgrowth/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,11 +17,43 @@ import (
 	"bucketgrowth"
 )
 
+const outputText string = "text"
+
+const outputJson string = "json"
+
 var flagProfile string
 
 var flagRegion string
 
 var flagVerbose bool
+
+var flagOutputType string
+
+var ErrUnsupportedOutputType error = errors.New("Unsupported output type")
+
+var validOutputTypes []string = []string{outputText, outputJson}
+
+func guardOutputType() error {
+	for _, val := range validOutputTypes {
+		if val == flagOutputType {
+			return nil // flag has been found, return no error
+		}
+	}
+
+	return ErrUnsupportedOutputType
+}
+
+func guardBucketArg(c *cli.Context, bucket string) error {
+	if bucket != "" {
+		return nil
+	}
+
+	if err := cli.ShowAppHelp(c); err != nil {
+		return err
+	}
+
+	return errors.New("") // blank error to show there's an issue
+}
 
 func perform(c *cli.Context) error {
 	if flagVerbose {
@@ -28,15 +62,20 @@ func perform(c *cli.Context) error {
 
 	bucket := c.Args().Get(0)
 
-	if bucket == "" {
-		if err := cli.ShowAppHelp(c); err != nil {
-      return err
-    }
-		return nil
+	if err := guardBucketArg(c, bucket); err != nil {
+		return err
+	}
+
+	if err := guardOutputType(); err != nil {
+		return err
 	}
 
 	if flagProfile != "" {
-		log.Printf("Using AWS profile: %s", flagProfile)
+		log.Printf("Using AWS profile: %s\n", flagProfile)
+	}
+
+	if flagRegion != "" {
+		log.Printf("Using AWS region: %s\n", flagRegion)
 	}
 
 	opts := session.Options{
@@ -55,19 +94,34 @@ func perform(c *cli.Context) error {
 		return err
 	}
 
-	displayMetrics(metrics)
+	if err := displayMetrics(metrics); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func displayMetrics(metrics bucketgrowth.Metrics) {
-	fmt.Println("Bucket Growth")
-	fmt.Println("=============")
-	fmt.Printf("Total Size: %s\n", humanize.Bytes(uint64(metrics.TotalSizeBytes)))
-	fmt.Printf("Total Objects: %s\n", humanize.Comma(metrics.TotalObjectCount))
-	fmt.Println("")
-	fmt.Printf("Size Growth: %s%%/mo, %s%%/yr\n", humanize.CommafWithDigits(metrics.SizeGrowthMonthly, 2), humanize.CommafWithDigits(metrics.SizeGrowthYearly, 2))
-	fmt.Printf("Object Growth: %s%%/mo, %s%%/yr\n", humanize.CommafWithDigits(metrics.ObjectGrowthMonthly, 2), humanize.CommafWithDigits(metrics.ObjectGrowthYearly, 2))
+func displayMetrics(metrics bucketgrowth.Metrics) error {
+	if flagOutputType == outputJson {
+		output, err := json.MarshalIndent(metrics, "", "  ")
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s\n", output)
+	}
+
+	if flagOutputType == outputText {
+		fmt.Println("Bucket Growth")
+		fmt.Println("=============")
+
+		fmt.Printf("Total Size: %s\n", humanize.Bytes(uint64(metrics.TotalSizeBytes)))
+		fmt.Printf("Total Objects: %s\n", humanize.Comma(metrics.TotalObjectCount))
+		fmt.Println("")
+		fmt.Printf("Size Growth: %s%%/mo, %s%%/yr\n", humanize.CommafWithDigits(metrics.SizeGrowthMonthly, 2), humanize.CommafWithDigits(metrics.SizeGrowthYearly, 2))
+		fmt.Printf("Object Growth: %s%%/mo, %s%%/yr\n", humanize.CommafWithDigits(metrics.ObjectGrowthMonthly, 2), humanize.CommafWithDigits(metrics.ObjectGrowthYearly, 2))
+	}
+
+	return nil
 }
 
 func main() {
@@ -94,6 +148,12 @@ func main() {
 				Usage:       "Enable verbose logging",
 				Aliases:     []string{"v"},
 				Destination: &flagVerbose,
+			},
+			&cli.StringFlag{
+				Name:        "output",
+				Usage:       fmt.Sprintf("Changes the output `TYPE` to %s or %s", outputText, outputJson),
+				Value:       outputText,
+				Destination: &flagOutputType,
 			},
 		},
 	}
--- growth.go
+++ growth.go
@@ -1,9 +1,9 @@
 package bucketgrowth
 
 import (
-  "fmt"
-  "log"
-  "time"
+	"fmt"
+	"log"
+	"time"
 )
 
 // Custom error for when a metric isn't found in an array of metrics
@@ -12,72 +12,72 @@ var ErrDailyMetricNotFound error = fmt.Errorf("Metric not found")
 // Returns the decimal value that equates to unit/month
 // Assumes array in already sorted by time (oldest->newest)
 func monthlyGrowthPct(metrics []DailyMetric) float64 {
-  var monthlyGrowthRates [12]float64
+	var monthlyGrowthRates [12]float64
 
-  for i, ending := 0, metrics[len(metrics)-1]; i < 12; i++ {
-    var starting DailyMetric
-    startingDate := ending.Date.AddDate(0, -1, 0)
-    starting, err := findMetricByDate(metrics, startingDate)
-    if err != nil {
-      starting = metrics[0]
-    }
-    log.Printf("[monthlyGrowth] Starting Date: %s\n", starting.Date)
-    log.Printf("[monthlyGrowth] Ending Date: %s\n", ending.Date)
-    growthRate := midpointGrowthRate(starting, ending)
+	for i, ending := 0, metrics[len(metrics)-1]; i < 12; i++ {
+		var starting DailyMetric
+		startingDate := ending.Date.AddDate(0, -1, 0)
+		starting, err := findMetricByDate(metrics, startingDate)
+		if err != nil {
+			starting = metrics[0]
+		}
+		log.Printf("[monthlyGrowth] Starting Date: %s\n", starting.Date)
+		log.Printf("[monthlyGrowth] Ending Date: %s\n", ending.Date)
+		growthRate := midpointGrowthRate(starting, ending)
 
-    monthlyGrowthRates[12-1-i] = growthRate
-    ending = starting
-  }
+		monthlyGrowthRates[12-1-i] = growthRate
+		ending = starting
+	}
 
-  log.Printf("Growth rates: %v\n", monthlyGrowthRates)
+	log.Printf("Growth rates: %v\n", monthlyGrowthRates)
 
-  avgMonthlyGrowthRate := average(monthlyGrowthRates[:])
+	avgMonthlyGrowthRate := average(monthlyGrowthRates[:])
 
-  log.Printf("[monthlyGrowth] Growth rate: %f\n", avgMonthlyGrowthRate)
+	log.Printf("[monthlyGrowth] Growth rate: %f\n", avgMonthlyGrowthRate)
 
-  return avgMonthlyGrowthRate * 100
+	return avgMonthlyGrowthRate * 100
 }
 
 func average(metrics []float64) float64 {
-  var sum float64
-  for _, val := range metrics {
-    sum += val
-  }
+	var sum float64
+	for _, val := range metrics {
+		sum += val
+	}
 
-  return sum / float64(len(metrics))
+	return sum / float64(len(metrics))
 }
 
 func findMetricByDate(metrics []DailyMetric, date time.Time) (DailyMetric, error) {
-  var metric DailyMetric
+	var metric DailyMetric
 	for i, val := range metrics {
 		if val.Date == date {
-      metric = metrics[i]
+			metric = metrics[i]
 			break
 		}
 	}
 
-  if metric == (DailyMetric{}) {
-    return DailyMetric{}, ErrDailyMetricNotFound
-  }
+	if metric == (DailyMetric{}) {
+		return DailyMetric{}, ErrDailyMetricNotFound
+	}
 
-  return metric, nil
+	return metric, nil
 }
 
 // Returns the decimal value that equates to unit/year
 // Assumes array in already sorted by time (oldest->newest)
 func yearlyGrowthPct(metrics []DailyMetric) float64 {
-  ending := metrics[len(metrics)-1]
-  starting := metrics[0]
+	ending := metrics[len(metrics)-1]
+	starting := metrics[0]
 
-  log.Printf("[yearlyGrowth] Starting value: %d\n", starting.Total)
-  log.Printf("[yearlyGrowth] Ending value: %d\n", ending.Total)
+	log.Printf("[yearlyGrowth] Starting value: %d\n", starting.Total)
+	log.Printf("[yearlyGrowth] Ending value: %d\n", ending.Total)
 
-  growthRate := midpointGrowthRate(starting, ending)
-  log.Printf("[yearlyGrowth] Growth rate: %f\n", growthRate)
+	growthRate := midpointGrowthRate(starting, ending)
+	log.Printf("[yearlyGrowth] Growth rate: %f\n", growthRate)
 
 	return float64(growthRate * 100)
 }
 
 func midpointGrowthRate(starting, ending DailyMetric) float64 {
-	return float64(ending.Total - starting.Total)/float64((starting.Total + ending.Total)/2)
+	return float64(ending.Total-starting.Total) / float64((starting.Total+ending.Total)/2)
 }
